08f7e1e9c37ba851aae957bc009f7307e80b2b52
Michał Berent <zaguada13@gmail.com>
2016-04-23 19:45:30 +0200

    base architecure MVC

---
+++ PrincessTowerDefence/Model/EnemyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincessTowerDefence.Model
+{
+    public class EnemyModel
+    {
+        public int HealthPoints { get; set; }
+        public EnemyType Type { get; set; }
+    }
+
+    public enum EnemyType
+    {
+        Spider,
+        Minion,
+        FrogKing
+    }
+}
---
+++ PrincessTowerDefence/Model/PrincessModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincessTowerDefence.Model
+{
+    public class PrincessModel
+    {
+        public int shootDelay;
+        public int range;
+        public PrincessType type;
+
+    }
+    public enum PrincessType
+    {
+        Blue,
+        Pink,
+        Yellow
+    }
+}
