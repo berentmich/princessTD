771775042ea78a6ea7fc73bd420b1d70c75515b9
Michał Berent <zaguada13@gmail.com>
2016-04-23 18:47:56 +0200

    first commit

---
+++ PrincessTowerDefence/Princesses/Princess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincessTowerDefence.Princesses
+{
+    enum PrincessType
+    {
+        Blue,
+        Pink,
+        Yellow
+    }
+
+    class Princess
+    {
+        private int shootDelay;
+        private int range;
+        PrincessType type;
+
+    }
+}
